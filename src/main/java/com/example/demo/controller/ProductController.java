c2d0a2f70fd284478c8507bd0593684d87399f65
Saurabh <saurabh.a@consultadd.com>
2024-07-05 22:21:28 +0530

    patch mapping

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -6,41 +6,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService service;
 
+
+//    Add one product
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product){
         return service.saveProduct(product);
     }
+//    Add multiple products
     @PostMapping("/addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
         return service.saveProducts(products);
     }
 
+
+//    Get all
     @GetMapping("/products")
     public List<Product> findAllProducts() {
         return service.getProducts();
     }
+//    Get by id
     @GetMapping("/productById/{id}")
     public Product findProductById(@PathVariable int id) {
         return service.getProductById(id);
     }
+//    Get by name
     @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         return service.getProductByName(name);
     }
 
+
+//    Update
     @PutMapping("/update")
     public Product updateProduct(@RequestBody Product product) {
         return service.updateProduct(product);
     }
 
+//    Delete
     @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable int id) {
         return service.deleteProduct(id);
+    }
+
+//    Patch
+    @PatchMapping("/patch/{id}")
+    public Product patchUpdate(@PathVariable int id, @RequestBody Map<String, Object> fields){
+//        System.out.println(fields);
+        return service.patchUpdate(id, fields);
     }
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -4,8 +4,11 @@ import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
@@ -13,21 +16,21 @@ public class ProductService {
     private ProductRepository repository;
 
 //    POST
-    public Product saveProduct (Product product) {
+    public Product saveProduct(Product product) {
         return repository.save(product);
     }
-    public List<Product> saveProducts (List<Product> products){
+    public List<Product> saveProducts(List<Product> products){
         return repository.saveAll(products);
     }
 
 //    GET
-    public List<Product> getProducts (){
+    public List<Product> getProducts(){
         return repository.findAll();
     }
     public Product getProductById(int id){
         return repository.findById(id).orElse(null);
     }
-    public Product getProductByName (String name) {
+    public Product getProductByName(String name) {
         return repository.findByName(name) ;
     }
 
@@ -43,6 +46,19 @@ public class ProductService {
         existingProduct.setName(product.getName());
         existingProduct.setQuantity(product.getQuantity());
         existingProduct.setPrice(product.getPrice());
+        return repository.save(existingProduct);
+    }
+
+//    PATCH
+    public Product patchUpdate(int id, Map<String, Object> fields){
+        Product existingProduct = repository.findById(id).get();
+
+        fields.forEach((key, value)->{
+            Field field = ReflectionUtils.findField(Product.class, key);
+            field.setAccessible(true);
+            ReflectionUtils.setField(field, existingProduct, value);
+        });
+
         return repository.save(existingProduct);
     }
 }
